b4674d1cc97ea72a31b3b68ee667921b220c664c
Mohammed Modather <mohammedmodather144@gmail.com>
2022-04-02 11:03:40 +0200

    Finished create section clients and modify css section main

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 $(function () {
   'use strict';
+    // ------------------------------------------------------------------------->
+  // trigger swiper scroll
+  new Swiper('.clients-swiper', {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
+  });
+
 });
